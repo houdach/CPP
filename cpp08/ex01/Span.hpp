d337d56d66b89ef57c01e4a3b477943c5bc5bb06
Houda Chouai <hchouai@e1r6p1.1337.ma>
2025-10-02 22:00:22 +0100

    without ex01

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -3,30 +3,27 @@
 
 #include <vector>
 #include <stdexcept>
-#include <algorithm> // for sort, min_element, etc.
+#include <algorithm> 
 
 class Span
 {
 private:
-    unsigned int _N;                 // Maximum capacity
-    std::vector<int> _numbers;       // Container to store integers
+    unsigned int _N;            
+    std::vector<int> _numbers;      
 
 public:
-    // Constructor / Destructor
+ 
     Span(unsigned int N);
     ~Span();
 
-    // Copy constructor and assignment operator
     Span(const Span& other);
     Span& operator=(const Span& other);
 
-    // Add numbers
-    void addNumber(int number); // add a single number
+    void addNumber(int number); 
 
     template <typename InputIt>
-    void addNumber(InputIt begin, InputIt end); // add a range of numbers
+    void addNumber(InputIt begin, InputIt end);
 
-    // Compute spans
     int shortestSpan() const;
     int longestSpan() const;
 };
